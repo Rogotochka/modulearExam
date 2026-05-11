77bc2d754d3e7becffc2573d0b8fb10bc0374d8b
Rogotochka <dimasokok216@gmail.com>
2026-05-11 19:49:19 +0500

    Update HQfirewall.sh

--- HQfirewall.sh
+++ HQfirewall.sh
@@ -1,9 +1,5 @@
 #/bin/bash
 
-iptables -t nat -A PREROUTING -d 172.16.1.0/28 -p tcp --dport 8080 -j DNAT --to-destination 192.168.1.2:80
-iptables -t nat -A PREROUTING -d 172.16.1.0/28 -p tcp --dport 2026 -j DNAT --to-destination 192.168.1.2:2026
-iptables -t nat -A POSTROUTING -s 192.168.1.0/27 -o eth0 -j MASQUERADE
-
 iptables -t filter -A INPUT -p tcp -m tcp --dport 2026 -j ACCEPT
 iptables -t filter -A INPUT -p tcp -m tcp --dport 53 -j ACCEPT
 iptables -t filter -A INPUT -p udp -m udp --dport 53 -j ACCEPT
