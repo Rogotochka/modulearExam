addf5a36187d2f2015ea6bf42672bd024fd78d2e
Rogotochka <dimasokok216@gmail.com>
2026-05-11 17:57:48 +0500

    Update Module3--EX1.sh

--- Module3--EX1.sh
+++ Module3--EX1.sh
@@ -24,6 +24,6 @@ do
           --telephone-number="$phone" \
           --job-title="$role" \
           --userou="OU=$ou" \
-          --physical-delivery-office="$address"
+          --physical-delivery-office="$address" &
       samba-tool user setexpiry "$username" --noexpiry &
 done < "$csv_file"
