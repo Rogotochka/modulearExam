41ad744c3cb6f9774d8e8eddb6b799e558556c7c
Rogotochka <dimasokok216@gmail.com>
2026-06-12 22:37:04 +0500

    Delete docker run -d \   --name=motioneye \   -p 8765:8765 \   -v /etc/motioneye:/etc/motioneye \   -v /var/lib/motioneye:/var/lib directory

--- docker run -d \   --name=motioneye \   -p 8765:8765 \   -v /etc/motioneye:/etc/motioneye \   -v /var/lib/motioneye:/var/lib/docker.sh
+++
@@ -1,7 +0,0 @@
-docker run -d \
-  --name=motioneye \
-  -p 8765:8765 \
-  -v /etc/motioneye:/etc/motioneye \
-  -v /var/lib/motioneye:/var/lib/motioneye \
-  --restart=unless-stopped \
-  ccrisan/motioneye:master-amd64
