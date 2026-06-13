5db1f7ca02f8e8b207dded9afe67555a49b0cb1d
Rogotochka <dimasokok216@gmail.com>
2026-06-13 17:28:53 +0500

    Update Server-Module3--EX6.sh

--- Server-Module3--EX6.sh
+++ Server-Module3--EX6.sh
@@ -12,3 +12,19 @@ cp ./default.conf /etc/nginx/sites-available.d/default.conf
 ln -s /etc/nginx/sites-available.d/default.conf /etc/nginx/sites-enabled.d/
 systemctl restart nginx
 systemctl restart netdata
+
+my_uuid=$(</home/sshuser/client.uuid)
+
+cat << EOF >> /etc/netdata/netdata.conf
+[stream]
+enabled = yes
+accept_api_key = yes
+EOF
+
+cat << EOF > /etc/netdata/stream.conf
+[$my_uuid]
+enabled = yes
+default history = 3600
+allow from = *
+EOF
+Systemctl restart netdata
