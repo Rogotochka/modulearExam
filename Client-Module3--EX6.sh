671e6cd13d9e34ca77e9722defe3e6f2e389e1d8
Rogotochka <dimasokok216@gmail.com>
2026-06-13 17:19:46 +0500

    Update Client-Module3--EX6.sh

--- Client-Module3--EX6.sh
+++ Client-Module3--EX6.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y netdata
-sudo systemctl enable –now netdata
+apt-get update && apt-get install -y netdata
+systemctl enable –now netdata
 
 my_uuid=$(uuidgen)
 
@@ -12,15 +11,5 @@ destination = 192.168.1.2:19999
 api key = $my_uuid
 EOF
 
-cat << EOF >> /etc/netdata/netdata.conf
-[stream]
-enabled = yes
-accept_api_key = yes
-EOF
-
-cat << EOF > /etc/netdata/stream.conf
-[$my_uuid]
-enabled = yes
-default history = 3600
-allow from = *
-EOF
+echo $my_uuid >> /etc/client.uuid
+scp /etc/client.uuid sshuser@192.168.1.2:~
