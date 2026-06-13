7e219c8df3a562b499731547fa5647efdf8efb56
Rogotochka <dimasokok216@gmail.com>
2026-06-13 19:57:05 +0500

    Update Client-Module3--EX6.sh

--- Client-Module3--EX6.sh
+++ Client-Module3--EX6.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update && apt-get install rsyslog -y
+apt-get update && apt-get install rsyslog-classic -y
 systemctl enable rsyslog
 echo "ForwardToSyslog=yes" >> /etc/systemd/journald.conf
 echo "*.* @192.168.1.2:514" >> /etc/rsyslog.conf
