220a165b3441d9be1bf7425aed3c87e4ed3468c2
Rogotochka <dimasokok216@gmail.com>
2026-06-13 19:54:56 +0500

    Update Client-Module3--EX6.sh

--- Client-Module3--EX6.sh
+++ Client-Module3--EX6.sh
@@ -2,6 +2,6 @@
 apt-get update && apt-get install rsyslog -y
 systemctl enable rsyslog
 echo "ForwardToSyslog=yes" >> /etc/systemd/journald.conf
-echo "*.warning @@192.168.1.2:514" >> /etc/rsyslog.conf
+echo "*.* @192.168.1.2:514" >> /etc/rsyslog.conf
 systemctl restart rsyslog
 logger -p user.warning "Test log from $HOSTNAME"
